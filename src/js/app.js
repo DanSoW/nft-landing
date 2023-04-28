09fffbf2c3c2f47e3de9c2cf3c092ed12e5f5058
DanSoW <67583913+DanSoW@users.noreply.github.com>
2023-04-28 11:11:02 +0800

    feat: Добавил секцию "How it works", исправил баги

--- src/js/app.js
+++ src/js/app.js
@@ -16,14 +16,15 @@ import AOS from 'aos';
 flsFunctions.isWebp();
 
 const swiper = new Swiper('.auctions-slider', {
-	loop: true,   // Бесконечная прокрутка
 	navigation: { // Добавление кнопок
 		nextEl: '.swiper-button-next1',
 		prevEl: '.swiper-button-prev1'
 	},
-	modules: [Navigation], // Используемые модули
+	modules: [Navigation, FreeMode], // Используемые модули
 	autoHeight: true, // Автоматическое вычисление высоты
 	speed: 500, // Скорость прокрутки
 	slidesPerView: 3, // Количество слайдов, которые будут видны
-	spaceBetween: 42
+	spaceBetween: 42,
+	freeMode: true,
+	grabCursor: true
 });
